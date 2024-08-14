13cc10310adfacbac8a5cfe79ea6875442efcf70
Jérôme Commaret <jcommaret@gmail.com>
2024-08-14 22:27:24 +0200

    refactor: Update OpenGraph and Twitter Card meta tags for social media sharing

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -2,7 +2,6 @@ import { Helmet } from 'react-helmet';
 
 import img from '../assets/img/images';
 
-const socialPreview = img.socialPreview;
 const urlsite = "https://jcommaret.github.io";
 const title = "Jérôme Commaret - Code, Manage, Innove";
 const description = "Jérôme Commaret, développeur React et manager technique passionné par l'innovation.";
@@ -11,20 +10,20 @@ function Head() {
   return (
     <Helmet>
       <title>{title}</title>
-      
-      <meta property="og:title" content={title} />
-      <meta property="og:type" content="website" />
-      <meta property="og:url" content={urlsite} />
-      <meta property="og:image" content={socialPreview} />
-      <meta property="og:description" content={description} />
-      <meta property="og:site_name" content="Portfolio de Jérôme Commaret" />
-      <meta name="author" content="Jérôme Commaret" />
-  
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:site" content="@jcommaret" />
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={socialPreview} />
+      <meta name="description" content="Jérôme Code, Manage, Innove"/>
+
+      <meta property="og:url" content="https://jcommaret.github.io/"/>
+      <meta property="og:type" content="website"/>
+      <meta property="og:title" content="Jérôme Commaret - Portfolio"/>
+      <meta property="og:description" content="Jérôme Code, Manage, Innove"/>
+      <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/89750128-d4c1-444f-b6b5-e41ee112b0c1.jpg?token=jnpzfGGTGRQ-VdJv0lLP9vppgkTSfDjNumq_3vbb1rc&height=630&width=1200&expires=33259666954"/>
+
+      <meta name="twitter:card" content="summary_large_image"/>
+      <meta property="twitter:domain" content="jcommaret.github.io"/>
+      <meta property="twitter:url" content="https://jcommaret.github.io/"/>
+      <meta name="twitter:title" content="Jérôme Commaret - Portfolio"/>
+      <meta name="twitter:description" content="Jérôme Code, Manage, Innove"/>
+      <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/89750128-d4c1-444f-b6b5-e41ee112b0c1.jpg?token=jnpzfGGTGRQ-VdJv0lLP9vppgkTSfDjNumq_3vbb1rc&height=630&width=1200&expires=33259666954"/>
     </Helmet>
   );
 }
--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -2,8 +2,8 @@ import { Outlet } from 'react-router-dom';
 
 import Sidebar from '../components/sidebar';
 import Navbar from '../components/navbar';
-import '../App.css';
 import Head from '../components/head';
+import '../App.css';
 
 function Root() {
   return (
