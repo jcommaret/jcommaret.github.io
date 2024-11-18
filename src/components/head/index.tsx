0b2f29837b277fa158199a60fe71fba76e5ff93d
Jérôme Commaret <jcommaret@gmail.com>
2024-11-18 14:50:58 +0100

    Head : update

--- src/components/head/index.tsx
+++ src/components/head/index.tsx
@@ -1,24 +1,29 @@
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function Head() {
+  const url = "https://jcommaret.github.io/";
+  const title = "Jérôme Commaret";
+  const description = "Jérôme Code, Manage, Innove";
+  const image = "https://opengraph.b-cdn.net/production/images/89750128-d4c1-444f-b6b5-e41ee112b0c1.jpg?token=jnpzfGGTGRQ-VdJv0lLP9vppgkTSfDjNumq_3vbb1rc&height=630&width=1200&expires=33259666954";
+
   return (
     <HelmetProvider>
       <Helmet>
-        <title>Jérôme Commaret</title>
-        <meta name="description" content="Jérôme Code, Manage, Innove"/>
+        <title>{title}</title>
+        <meta name="description" content={description}/>
 
-        <meta property="og:url" content="https://jcommaret.github.io/"/>
+        <meta property="og:url" content={url}/>
         <meta property="og:type" content="website"/>
-        <meta property="og:title" content="Jérôme Commaret - Portfolio"/>
-        <meta property="og:description" content="Jérôme Code, Manage, Innove"/>
-        <meta property="og:image" content="https://opengraph.b-cdn.net/production/images/89750128-d4c1-444f-b6b5-e41ee112b0c1.jpg?token=jnpzfGGTGRQ-VdJv0lLP9vppgkTSfDjNumq_3vbb1rc&height=630&width=1200&expires=33259666954"/>
+        <meta property="og:title" content={`${title} - Portfolio`}/>
+        <meta property="og:description" content={description}/>
+        <meta property="og:image" content={image}/>
 
         <meta name="twitter:card" content="summary_large_image"/>
-        <meta property="twitter:domain" content="jcommaret.github.io"/>
-        <meta property="twitter:url" content="https://jcommaret.github.io/"/>
-        <meta name="twitter:title" content="Jérôme Commaret - Portfolio"/>
-        <meta name="twitter:description" content="Jérôme Code, Manage, Innove"/>
-        <meta name="twitter:image" content="https://opengraph.b-cdn.net/production/images/89750128-d4c1-444f-b6b5-e41ee112b0c1.jpg?token=jnpzfGGTGRQ-VdJv0lLP9vppgkTSfDjNumq_3vbb1rc&height=630&width=1200&expires=33259666954"/>
+        <meta property="twitter:domain" content={url}/>
+        <meta property="twitter:url" content={url}/>
+        <meta name="twitter:title" content={`${title} - Portfolio`}/>
+        <meta name="twitter:description" content={description}/>
+        <meta name="twitter:image" content={image}/>
       </Helmet>
     </HelmetProvider>
   );
