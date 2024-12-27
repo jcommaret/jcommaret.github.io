67705d20274d66f7684e22fbc37a81960e7062c6
Jérôme Commaret <jcommaret@gmail.com>
2024-12-27 18:49:24 +0100

    Refactor About page: standardize import statements, improve code formatting, and enhance testimonials and services sections for better readability and functionality.

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,110 +1,117 @@
 // External dependencies
-import { IonIcon } from '@ionic/react';
-import { addOutline } from 'ionicons/icons';
+import { IonIcon } from "@ionic/react";
+import { addOutline } from "ionicons/icons";
 
 // Internal components & hooks
-import Modal from '../../components/modal';
-import { useModal } from '../../hooks/useModal';
+import Modal from "../../components/modal";
+import { useModal } from "../../hooks/useModal";
 
 // Types
-import { Testimonial } from '../../types/types';
+import { Testimonial } from "../../types/types";
 
 // Assets
-import img from '../../assets/img/images';
+import img from "../../assets/img/images";
 
 // Data
-import aboutData from '../../data/about.json';
+import aboutData from "../../data/about.json";
 
 // Styles
-import './index.scss';
-
+import "./index.scss";
 
 function About() {
-    const { 
-        isModalOpen, 
-        selectedItem: selectedTestimonial, 
-        openModal, 
-        closeModal 
-    } = useModal<Testimonial>({
-        image: '',
-        title: '',
-        content: {
-            text: ''
-        }
-    });
+  const {
+    isModalOpen,
+    selectedItem: selectedTestimonial,
+    openModal,
+    closeModal,
+  } = useModal<Testimonial>({
+    image: "",
+    title: "",
+    content: {
+      text: "",
+    },
+  });
 
-    return(
-        <article className="about active" data-page="about">
-            <header>
-                <h2>A propos</h2>
-            </header>
+  return (
+    <article className="about active" data-page="about">
+      <header>
+        <h2>A propos</h2>
+      </header>
 
-            <section className="about-text">
-                {aboutData.aboutText.map((text, index) => (
-                    <p key={index}>{text}</p>
-                ))}
-            </section>
+      <section className="about-text">
+        {aboutData.aboutText.map((text, index) => (
+          <p key={index}>{text}</p>
+        ))}
+      </section>
 
-            <section className="service">
-                <h3>Activités</h3>
-                <ul className="service-list">
-                    {aboutData.services.map((service, index) => (
-                        <li key={index} className="service-item">
-                            <div className="service-icon-box">
-                                <img className='service-icon-icon' src={img[service.icon as keyof typeof img]} alt="Web development icon" width="40" />
-                            </div>
-                            <div className="service-content-box">
-                                <h4 className="service-item-title">{service.title}</h4>
-                                <p className="service-item-text">{service.description}</p>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </section>
-        
-            <section className="testimonials">
-                <h3>Témoignages</h3>
-                <ul className="testimonials-list">
-                    {aboutData.testimonials.map((testimonial, index) => (
-                        <li key={index} className="testimonials-item">
-                            <div 
-                                className="content-card" 
-                                data-testimonials-item 
-                                onClick={() => openModal({
-                                    image: img[testimonial.image as keyof typeof img],
-                                    title: testimonial.title,
-                                    content: testimonial.content
-                                })}
-                            >
-                                <figure className="testimonials-avatar-box">
-                                    <img 
-                                        src={img[testimonial.image as keyof typeof img]} 
-                                        alt={testimonial.title} 
-                                        data-testimonials-avatar 
-                                    />
-                                </figure>
-                                <h4 className="testimonials-item-title">
-                                    {testimonial.title}
-                                </h4>
-                                <div className="testimonials-text">
-                                    <p>{testimonial.content.text}</p>
-                                </div>
-                                <div className="testimonials-expand-icon-wrapper">
-                                    <IonIcon icon={addOutline} className="testimonials-expand-icon" />
-                                </div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-            </section>
-        
-            <Modal 
-                isOpen={isModalOpen}
-                onClose={closeModal}
-                type="testimonial"
-                data={selectedTestimonial}
-            />
-        </article> 
-    );
+      <section className="service">
+        <h3>Activités</h3>
+        <ul className="service-list">
+          {aboutData.services.map((service, index) => (
+            <li key={index} className="service-item">
+              <div className="service-icon-box">
+                <img
+                  className="service-icon-icon"
+                  src={img[service.icon as keyof typeof img]}
+                  alt="Web development icon"
+                  width="40"
+                />
+              </div>
+              <div className="service-content-box">
+                <h4 className="service-item-title">{service.title}</h4>
+                <p className="service-item-text">{service.description}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className="testimonials">
+        <h3>Témoignages</h3>
+        <ul className="testimonials-list">
+          {aboutData.testimonials.map((testimonial, index) => (
+            <li key={index} className="testimonials-item">
+              <div
+                className="content-card"
+                data-testimonials-item
+                onClick={() =>
+                  openModal({
+                    image: img[testimonial.image as keyof typeof img],
+                    title: testimonial.title,
+                    content: testimonial.content,
+                  })
+                }
+              >
+                <figure className="testimonials-avatar-box">
+                  <img
+                    src={img[testimonial.image as keyof typeof img]}
+                    alt={testimonial.title}
+                    data-testimonials-avatar
+                  />
+                </figure>
+                <h4 className="testimonials-item-title">{testimonial.title}</h4>
+                <div className="testimonials-text">
+                  <p>{testimonial.content.text}</p>
+                </div>
+                <div className="testimonials-expand-icon-wrapper">
+                  <IonIcon
+                    icon={addOutline}
+                    className="testimonials-expand-icon"
+                  />
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <Modal
+        isOpen={isModalOpen}
+        onClose={closeModal}
+        type="testimonial"
+        data={selectedTestimonial}
+      />
+    </article>
+  );
 }
 export default About;
