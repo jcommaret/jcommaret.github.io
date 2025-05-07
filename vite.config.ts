83fd3ac1ab575e9d60cb47053b1c91fcc30d148b
Jérôme Commaret <jcommaret@gmail.com>
2025-05-07 03:37:35 +0200

    Changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
+import sitemap from 'vite-plugin-sitemap'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ 
+      hostname: 'https://jcommaret.github.io', 
+      dynamicRoutes: [
+        '/',
+        '/#/about',
+        '/#/projects',
+        '/#/resume'
+      ]
+     })
+  ],
   base: "",
   build: {
     assetsDir: "assets",
